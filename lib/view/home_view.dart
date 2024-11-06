5a7d0c1aadd58727a37406c676d4a9653e1e91be
HaiderAli228 <flutter2830@gmail.com>
2024-11-06 19:37:53 +0500

    Setting the UI

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -8,7 +8,7 @@ import 'package:snapmotion/utils/app_color.dart';
 import '../model-view/video_viewmodel.dart';
 
 class HomeView extends StatefulWidget {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
 
   @override
   HomeViewState createState() => HomeViewState();
@@ -61,45 +61,107 @@ class HomeViewState extends State<HomeView> {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: AppColor.themeColor,
-        title: const Text("Image to Video Generator"),
+        title: const Text(
+          "Image to Video Generator",
+          style: TextStyle(
+              color: AppColor.themeTextColor,
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.bold),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (_selectedImage != null)
-              Image.file(
-                _selectedImage!,
-                height: 300,
-                width: 300,
-                fit: BoxFit.cover,
-              )
-            else
-              Container(
-                height: 300,
-                width: 300,
-                color: Colors.grey[200],
-                child: const Icon(Icons.image, size: 100),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (_selectedImage != null)
+                Image.file(
+                  _selectedImage!,
+                  height: 300,
+                  width: 300,
+                  fit: BoxFit.cover,
+                )
+              else
+                Container(
+                  height: 300,
+                  width: 300,
+                  color: Colors.grey[200],
+                  child: const Icon(Icons.image, size: 100),
+                ),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                      onTap: () => _captureImage,
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: const BoxDecoration(
+                            color: AppColor.themeColor,
+                            borderRadius: BorderRadius.all(Radius.circular(8))),
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.all(4),
+                        margin: const EdgeInsets.symmetric(horizontal: 4),
+                        child: const Icon(
+                          Icons.camera_alt_rounded,
+                          size: 32,
+                          color: AppColor.themeTextColor,
+                        ),
+                      )),
+                  InkWell(
+                    onTap: _pickImageFromGallery,
+                    child: Container(
+                      height: 50,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 14, vertical: 1),
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 3, vertical: 8),
+                      alignment: Alignment.center,
+                      decoration: const BoxDecoration(
+                        color: AppColor.themeColor,
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                      child: const Text(
+                        "Pick image from gallery",
+                        style: TextStyle(
+                            color: AppColor.themeTextColor,
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ),
+                  )
+                ],
               ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _pickImageFromGallery,
-              child: const Text("Pick Image from Gallery"),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: _captureImage,
-              child: const Text("Capture Image with Camera"),
-            ),
-            const SizedBox(height: 20),
-            viewModel.isLoading
-                ? const CircularProgressIndicator()
-                : ElevatedButton(
-                    onPressed: () => _generateVideo(context),
-                    child: const Text("Generate Video"),
-                  ),
-          ],
+              const SizedBox(height: 10),
+              viewModel.isLoading
+                  ? const CircularProgressIndicator()
+                  : InkWell(
+                      onTap: () => _generateVideo(context),
+                      child: Container(
+                        height: 50,
+                        width: 307,
+                        padding: const EdgeInsets.all(4),
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        alignment: Alignment.center,
+                        decoration: const BoxDecoration(
+                          color: AppColor.themeColor,
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                        ),
+                        child: const Text(
+                          "Generate Video",
+                          style: TextStyle(
+                              color: AppColor.themeTextColor,
+                              fontFamily: "Poppins",
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                      ),
+                    )
+            ],
+          ),
         ),
       ),
     );
